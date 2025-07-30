7fd255cb5e29e8ab270a71e098d480bdef2f51e7
Guillermo2070 <guillermoquintero2070@gmail.com>
2025-07-30 14:36:34 -0500

    Integracion Continua Entrega Final - Prueba Funcional - 1

--- ChatApi/obj/Debug/net8.0/ChatApi.AssemblyInfo.cs
+++ ChatApi/obj/Debug/net8.0/ChatApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChatApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+381ae76af53bf678d622023cd85e15f467360e53")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+54874413f22634618790fdfd71b0ba23a1d5f394")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChatApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChatApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
